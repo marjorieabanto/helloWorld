1fcff92b061fbd2893a5a43ca5212873f36d7c66
marjorieabanto <u202118523@upc.edu.pe>
2024-08-09 01:41:39 -0500

    JAR Add

--- src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.helloWorld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class HelloWorldApplicationTests {
 
 	@Test
